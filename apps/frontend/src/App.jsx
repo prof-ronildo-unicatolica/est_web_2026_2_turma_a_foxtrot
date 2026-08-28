47dcb5a609028eabe2f5b1e5b14c82a72620afd8
OlivLucasNascimento <olivlucasnascimento@gmail.com>
2026-08-28 20:26:29 -0300

    Revert "feat: criar navbar, rotas e cliente http"
    This reverts commit c2008c678dc915ec3a3d9b5eee18373d464fc75e.

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,24 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
 function App() {
   return (
-    <BrowserRouter>
-      {/* A Navbar fica fora das rotas para aparecer fixada no topo em todas as telas */}
-      <Navbar />
-      
-      <div style={{ padding: '2rem' }}>
-        <Routes>
-          {/* Rotas Públicas (Hóspedes) */}
-          <Route path="/" element={<h2>Página Inicial - Bem-vindo à Rede Hoteleira!</h2>} />
-          <Route path="/hoteis" element={<h2>Lista de Hotéis Disponíveis</h2>} />
-          
-          {/* Rotas Privadas (Gestão) */}
-          <Route path="/admin" element={<h2>Dashboard da Franquia (Acesso Restrito)</h2>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <h1>Sistema de Reservas - Setup Frontend</h1>
+    </div>
   );
 }
 
 export default App;
+  
